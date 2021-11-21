6bc1c153488a6bb7e026eacadb6c82eb77413ea3
ramil <pashaevra@gmail.com>
2021-11-21 12:02:01 +0300

    ДЗ 9_3

---
+++ Lesson3/src/main/java/ru/gb/task1/Main.java
@@ -0,0 +1,8 @@
+package ru.gb.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        PingPong pingPongMachine = new PingPong();
+
+    }
+}
---
+++ Lesson3/src/main/java/ru/gb/task1/PingPong.java
@@ -0,0 +1,18 @@
+package ru.gb.task1;
+
+public class PingPong {
+    private boolean pingPongValue;
+
+    {
+        new Thread(() -> {
+            while (true) {
+                try {
+                    if (!pingPongValue) System.out.println("Ping");
+                    else System.out.println("Pong");
+                    Thread.sleep(1000);
+                    pingPongValue = !pingPongValue;
+                } catch (InterruptedException e) {}
+            } }).start();
+    }
+
+}
